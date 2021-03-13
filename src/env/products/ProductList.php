3d36d0e84e730c535bdd7dc0b568f0dea3010555
Miroslav Štolfa <miroslav.stolfa@ataccama.com>
2021-03-13 15:41:29 +0100

    product list update

--- src/env/products/ProductList.php
+++ src/env/products/ProductList.php
@@ -3,16 +3,23 @@
 
     namespace Stolfam\Eshop\Env\Products;
 
+    use Nette\SmartObject;
     use Stolfam\Arrays\BaseArray;
+    use Stolfam\Eshop\Utils\Currency;
+    use Stolfam\Eshop\Utils\Price;
 
 
     /**
      * Class ProductList
      *
      * @package Stolfam\Eshop\Env\Products
+     * @property-read Price $price
      */
     class ProductList extends BaseArray
     {
+        use SmartObject;
+
+
         /**
          * @param Product $product
          *
@@ -31,5 +38,18 @@
         public function current(): ?Product
         {
             return parent::current();
+        }
+
+        /**
+         * @return Price
+         */
+        public function getPrice(): Price
+        {
+            $price = new Price(0, Currency::getDefaultCurrency());
+            foreach ($this as $product) {
+                $price->plus($product->price);
+            }
+
+            return $price;
         }
     }
